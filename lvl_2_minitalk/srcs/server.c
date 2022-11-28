aa41c7ac7d7cf5912ec4457b32dae7926e13eb01
Nuno Miguel Nuno Carvalho <nnuno-ca@c1r15s14.42porto.com>
2022-11-28 18:16:53 +0000

    Updates

--- lvl_2_minitalk/srcs/server.c
+++ lvl_2_minitalk/srcs/server.c
@@ -3,22 +3,59 @@
 /*                                                        :::      ::::::::   */
 /*   server.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nnuno-ca <nnuno-ca@student.42porto.com>    +#+  +:+       +#+        */
+/*   By: nnuno-ca <nnuno-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/27 23:15:39 by nnuno-ca          #+#    #+#             */
-/*   Updated: 2022/11/27 23:25:13 by nnuno-ca         ###   ########.fr       */
+/*   Updated: 2022/11/28 17:57:50 by nnuno-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minitalk.h"
 
-int main(void)
+void	handle_sigusr(int signum)
+{
+	static int				bit_itr = -1;
+	static unsigned char	c;
+
+	if (bit_itr < 0)
+		bit_itr = 7;
+	if (signum == SIGUSR1)
+		c |= (1 << bit_itr);
+	bit_itr--;
+	if (bit_itr < 0 && c)
+	{
+		ft_putchar_fd(c, 1);
+		c = 0;
+	}
+}
+
+int	main(void)
 {
 	int	pid;
 
 	pid = getpid();
 	ft_printf("SERVER PID = %d\n", pid);
-	
-	
+	while (1)
+	{
+		signal(SIGUSR1, handle_sigusr);
+		signal(SIGUSR2, handle_sigusr);
+	}
 	return (EXIT_SUCCESS);
 }
+/*
+	if (signum == SIGUSR1)
+		write(1, "1", 1);
+	else if (signum == SIGUSR2)
+		write(1, "0", 1);
+
+	
+	if (signum == SIGUSR1)
+		c |= (1 << bit_itr);
+	bit_itr--;
+	if (bit_itr < 0 && c)
+    {
+        printf("C = %c\n", c);
+        c = 0;
+    }
+
+*/
